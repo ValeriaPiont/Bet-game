7e876f48ff55cb3afb8d3607a5b52401982dbd8b
Valeria Piont <iceflame.vvv@gmail.com>
2023-10-19 12:59:53 +0300

    task: added api documentation

--- src/main/java/com/test/yolo/controllers/GameController.java
+++ src/main/java/com/test/yolo/controllers/GameController.java
@@ -3,8 +3,14 @@ package com.test.yolo.controllers;
 import com.test.yolo.dto.BetRequestDTO;
 import com.test.yolo.dto.BetResponseDTO;
 import com.test.yolo.services.GameService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,12 +22,24 @@ import java.util.concurrent.CompletableFuture;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("/api/v1/game")
+@RequestMapping(value = "/api/v1/game", produces = MediaType.APPLICATION_JSON_VALUE)
 public class GameController {
 
     private final GameService gameService;
 
-    @PostMapping("/bet")
+    @PostMapping(value = "/bet", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Place a bet in the game")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Bet placed successfully",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = BetResponseDTO.class)
+                    )),
+            @ApiResponse(responseCode = "400", description = "Invalid request payload"),
+            @ApiResponse(responseCode = "500", description = "Internal server error occurred")
+    })
     public CompletableFuture<ResponseEntity<BetResponseDTO>> placeBet(@RequestBody @Valid BetRequestDTO request) {
         return gameService.calculateWinAsync(request.getBet(), request.getNumber())
                 .thenApply(win -> {
